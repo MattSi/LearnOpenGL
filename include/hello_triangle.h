d8546e667538262239f781006b71791702e58843
matt si <mian.si@outlook.com>
2024-01-03 14:54:47 +0800

    OpenGL Project

---
+++ include/hello_triangle.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_TRIANGLE
+#define HELLO_TRIANGLE
+
+
+int hello_triangle(int argc, char* argv[]);
+#endif // !HELLO_TRIANGLE
+
---
+++ include/hello_triangle_indexed.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_TRIANGLE_INDEXED
+#define HELLO_TRIANGLE_INDEXED
+
+
+int hello_triangle_indexed(int argc, char* argv[]);
+#endif // !HELLO_TRIANGLE_INDEXED
+
