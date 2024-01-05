116ae38894011ebecfabd2043eae980dac1318d2
Matt Si <mian.si@outlook.com>
2024-01-06 00:08:54 +0800

    add camera class -- to be done

--- include/camera.h
+++ include/camera.h
@@ -1,5 +1,36 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+class Camera {
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	// euler angle
+	float Yaw;
+	float Pitch;
+
+	// camera options
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+};
 #endif
