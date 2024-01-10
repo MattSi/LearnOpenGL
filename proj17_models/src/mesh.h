3366763455bc1505160edac1a0fd3618c319d8ab
Matt Si <mian.si@outlook.com>
2024-01-10 21:08:08 +0800

    fix issue to display models

--- proj17_models/src/mesh.h
+++ proj17_models/src/mesh.h
@@ -99,11 +99,11 @@ private:
 		// A great thing about structs is that their memory layout is sequential for all its items.
 	    // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
 	    // again translates to 3/2 floats which translates to a byte array.
-		glBufferData(GL_TRIANGLES, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
 
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_TRIANGLES, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
 
 		// set the vertex attribute pointers
@@ -131,6 +131,6 @@ private:
 		glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_Weights));
 
 		glBindVertexArray(0);
-	};
+	}
 };
 #endif // MESH_H
