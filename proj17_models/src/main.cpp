58160264ef9ccb955888ea054a22a052fec4a5c2
Matt Si <mian.si@outlook.com>
2024-01-10 20:47:33 +0800

    adding model files - to be correct

---
+++ proj17_models/src/main.cpp
@@ -0,0 +1,173 @@
+#include <Windows.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <camera.h>
+#include <shader_m.h>
+#include "filesystem.h"
+#include "stb_image.h"
+#include "model.h"
+
+
+static void resize(GLFWwindow* window, int width, int height);
+static void process_input(GLFWwindow* window);
+static void process_mouse(GLFWwindow* window, double xposIn, double yposIn);
+static void process_scroll(GLFWwindow* window, double xoffset, double yoffset);
+static GLuint loadTexture(const char* path);
+static const unsigned int SCR_WIDTH = 1024;
+static const unsigned int SCR_HEIGHT = 768;
+
+
+// camera 
+static Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+static float lastX = SCR_WIDTH / 2.0f;
+static float lastY = SCR_HEIGHT / 2.0f;
+static bool firstMouse = true;
+
+static float deltaTime = 0.0f;
+static float lastFrame = 0.0f;
+static glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+static glm::vec3 lightDirection(-0.2f, -1.0f, -0.3f);
+ 
+
+int main(int argc, char* argv[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Project 15, LightCasting - direction", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, resize);
+    glfwSetCursorPosCallback(window, process_mouse);
+    glfwSetScrollCallback(window, process_scroll);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+    glEnable(GL_DEPTH_TEST);
+
+    Shader ourShader("model_loading.vs", "model_loading.fs");
+    // load models
+     // -----------
+    Model ourModel(FileSystem::getPath("resources/objects/backpack/backpack.obj"));
+
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        process_input(window);
+
+        // render 
+         // ------
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+        ourShader.use();
+
+        // pass projection matrix to shader
+        glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", proj);
+        ourShader.setMat4("view", view);
+
+
+        // world transformation
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        ourShader.setMat4("model", model);
+
+        ourModel.Draw(ourShader);
+        
+ 
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+        lightDirection.x += 0.001f;
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+        lightDirection.x -= 0.001f;
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+        lightDirection.y += 0.001f;
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+        lightDirection.y -= 0.001f;
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+        lightDirection.z += 0.001f;
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+        lightDirection.z -= 0.001f;
+}
+
+static void resize(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+static void process_mouse(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float> (yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+static void process_scroll(GLFWwindow* window, double xoffset, double yoffset) {
+    float f_yoffset = static_cast<float>(yoffset);
+    camera.ProcessMouseScroll(f_yoffset);
+}
+ 
+
