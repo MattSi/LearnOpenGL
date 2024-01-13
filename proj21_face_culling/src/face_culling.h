7d2c18b72f57c6c4eb37458b42e23a500d847702
Matt Si <mian.si@outlook.com>
2024-01-13 16:25:43 +0800

    face culling added

---
+++ proj21_face_culling/src/face_culling.h
@@ -0,0 +1,5 @@
+#ifndef FACE_CULLING_H
+#define FACE_CULLING_H
+
+int face_culling(int argc, char* argv[]);
+#endif // !FACE_CULLING_H
---
+++ proj21_face_culling/src/main.cpp
@@ -0,0 +1,8 @@
+#include "face_culling.h"
+
+int main(int argc, char* argv[]) {
+
+	face_culling(argc, argv);
+	 
+	return 0;
+}
